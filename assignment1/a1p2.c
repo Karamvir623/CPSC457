a3d80fc983282844efc524053a27d00681f9727b
samiha030331 <145374121+samiha030331@users.noreply.github.com>
2025-09-16 17:16:30 -0600

    Update a1p2.c

--- assignment1/a1p2.c
+++ assignment1/a1p2.c
@@ -4,30 +4,41 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <math.h> // ask if we can use ceil/floor function
+#include <math.h>
+
 
 int main() {
+	//memory
     //key_t key = ftok("shmfile", 10); //unique key for shared memory
     //int shmid = shmget(key, 1024, IPC_CREAT | 0666); //from sharedMem.c
     //int *shm_ptr = (int *) shmat(shmid, NULL, 0); //assignment code
 
-    int upperbound;
-    int lowerbound;
-    int nprocess;
+	//parent child communication
+	int toParent;
+	int fromChild;
 
-    int toParent;
-	  int fromChild;
+	//example input
+	int upperbound = 200; 
+	int lowerbound = 100; 
+	int n = 3; 
 
-    //for testing
-    printf("Enter lower bound: ");
-    scanf("%d", &lowerbound);
+	
+	//logic for assigning ranges for each process
+	int range = upperbound - lowerbound;
+	int n_section = range/n;
+	
+	int sub_lowerbound;
+	int sub_upperbound;
+	
+	int i;
+	for(i = 1; i <= 1; i++){
+		sub_lowerbound = lowerbound;
+		sub_upperbound = sub_lowerbound + (i * n_section);
+		printf("%d, %d\n",sub_lowerbound,sub_upperbound);
+	}
 
-    printf("Enter upper bound: ");
-    scanf("%d", &upperbound);
 
-    printf("Enter number of N processes performed: ");
-    scanf("%d", &nprocess);
-
+   
     // from assignment page
     int is_prime(int num) {
         if (num < 2) return 0;
@@ -37,23 +48,23 @@ int main() {
         return 1;
       }
 
-    // assigning ranges to each process logic
     
-    pid_t pid = fork();
-
-    if (pid < 0) {
-		fprintf(stderr, "Fork Failed");
-		exit(-1);
-    }
     
-    else if (pid == 0) {
-        sleep(1); // waiting so parent doesn't execute  
-	  }
+  //   pid_t pid = fork();
 
-    else {
-        wait(&fromChild);
-		printf("done", WEXITSTATUS(fromChild));
-    }
+  //   if (pid < 0) {
+		// fprintf(stderr, "Fork Failed");
+		// exit(-1);
+  //   }
+    
+  //   else if (pid == 0) {
+  //       sleep(1); // waiting so parent doesn't execute  
+	 //  }
+
+  //   else {
+  //       wait(&fromChild);
+		// printf("done", WEXITSTATUS(fromChild));
+  //   }
 
     return 0;
 }
