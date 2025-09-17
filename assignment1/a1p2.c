45141731a1dd4ca0bed3459fbd7efa8c789b3a25
samiha030331 <145374121+samiha030331@users.noreply.github.com>
2025-09-16 18:49:41 -0600

    Update a1p2.c

--- assignment1/a1p2.c
+++ assignment1/a1p2.c
@@ -6,7 +6,6 @@
 #include <sys/wait.h>
 #include <math.h>
 
-
 int main() {
 	//memory
     //key_t key = ftok("shmfile", 10); //unique key for shared memory
@@ -22,7 +21,7 @@ int main() {
 	int lowerbound = 100; 
 	int n = 3; 
 
-	
+
 	//logic for assigning ranges for each process
 	int range = upperbound - lowerbound;
 	int n_section = range/n;
@@ -32,53 +31,48 @@ int main() {
 	
 	int i;
 	for(i = 1; i <= n; i++){
-		if( i == 1){
-		sub_lowerbound = lowerbound;
-		sub_upperbound = sub_lowerbound + (i * n_section);
-		printf("%d, %d\n",sub_lowerbound, sub_upperbound);
+		
+		pid_t pid = fork(); //Child Process
+
+		if (pid < 0){
+			fprintf(stderr,"Fork Failed");
+			exit(1);
 		}
-		else if (i > 1 & i < n){
-		sub_lowerbound = lowerbound + ((i-1) * n_section) + 1;
-		sub_upperbound = lowerbound + ((i) * n_section);
-		printf("%d, %d\n",sub_lowerbound, sub_upperbound);
+
+		else if(pid == 0){
+
+			if(i == 1){
+				sub_lowerbound = lowerbound;
+				sub_upperbound = sub_lowerbound + (i * n_section);
+			}
+				
+			else if (i > 1 & i < n){
+				sub_lowerbound = lowerbound + ((i-1) * n_section) + 1;
+				sub_upperbound = lowerbound + ((i) * n_section);
+			}
+			
+			else if (i == n){
+				sub_lowerbound = lowerbound + ((i-1) * n_section) + 1;
+				sub_upperbound = upperbound;
+			}
+			printf("Child %d checking range [%d, %d]\n", getpid(), sub_lowerbound, sub_upperbound);
+            exit(0);
 		}
-		else if (i == n){
-		sub_lowerbound = lowerbound + ((i-1) * n_section) + 1;
-		sub_upperbound = upperbound;
-		printf("%d, %d\n",sub_lowerbound, sub_upperbound);
-		}
-	}
 
-
-
-
-   
     // from assignment page
-    int is_prime(int num) {
-        if (num < 2) return 0;
-            for (int i = 2; i <= sqrt(num); i++) {
-                if (num % i == 0) return 0;
-            }
-        return 1;
-      }
+    // int is_prime(int num) {
+    //     if (num < 2) return 0;
+    //         for (int i = 2; i <= sqrt(num); i++) {
+    //             if (num % i == 0) return 0;
+    //         }
+    //     return 1;
+    //   }
 
-    
-    
-  //   pid_t pid = fork();
-
-  //   if (pid < 0) {
-		// fprintf(stderr, "Fork Failed");
-		// exit(-1);
-  //   }
-    
-  //   else if (pid == 0) {
-  //       sleep(1); // waiting so parent doesn't execute  
-	 //  }
-
-  //   else {
-  //       wait(&fromChild);
-		// printf("done", WEXITSTATUS(fromChild));
-  //   }
+	// Parent waits for all n number of children
+    for (int i = 0; i < n; i++) {
+        wait(NULL);
+    }
+    printf("All children finished.\n");
 
     return 0;
 }
