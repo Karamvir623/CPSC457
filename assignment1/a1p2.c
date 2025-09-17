21b8eaa94adc70c867e5b56400add1be30c315d0
samiha030331 <145374121+samiha030331@users.noreply.github.com>
2025-09-17 14:03:18 -0600

    Update a1p2.c

--- assignment1/a1p2.c
+++ assignment1/a1p2.c
@@ -14,7 +14,7 @@
 // handle cases when N < number of range values
 
 
-#define MAX_PRIMES_PER_CHILD 1000000 //ask about what range should be appropriate
+#define MAX_PRIMES_PER_CHILD 100000 //ask about what range should be appropriate
 
 // function to test if input is a prime number
 int is_prime(int num) {
@@ -26,18 +26,18 @@ int is_prime(int num) {
 }
 
 int main() {
-	//memory layout suggested by assignment sheet
-	int shm_size =  (n * MAX_PRIMES_PER_CHILD) * sizeof(int) // shared memory segment size
-	int shmid = shmget(IPC_PRIVATE, SIZE, IPC_CREAT | 0666); // shared memory creation
-	int *shm_ptr = (int *) shmat(shmid, NULL, 0) // attaches shared memory block
-	
 	//example input
 	int upperbound = 1000; 
 	int lowerbound = 100; 
 	int n = 3; 
 
+	//memory layout suggested by assignment sheet
+	int shm_size =  (n * MAX_PRIMES_PER_CHILD) * sizeof(int); // shared memory segment size
+	int shmid = shmget(IPC_PRIVATE, shm_size, IPC_CREAT | 0666); // shared memory creation
+	int *shm_ptr = (int *) shmat(shmid, NULL, 0); // attaches shared memory block
+
 	//logic for assigning ranges for each process
-	int range = upperbound - lowerbound;
+	int range = (upperbound - lowerbound) + 1;
 	int n_section = range/n;
 	
 	int sub_lowerbound;
@@ -47,29 +47,48 @@ int main() {
 		
 		pid_t pid = fork(); //Child Process
 		
+		//ERROR
 		if (pid < 0){
 			fprintf(stderr,"Fork Failed");
 			exit(1);
 		}
+
+		//CHILD
 		else if(pid == 0){
 
 			if(i == 1){
 				sub_lowerbound = lowerbound;
-				sub_upperbound = sub_lowerbound + (i * n_section);
+				sub_upperbound = sub_lowerbound + (i * n_section) - 1;
 			}
-				
-			else if (i > 1 & i < n){
-				sub_lowerbound = lowerbound + ((i-1) * n_section) + 1;
-				sub_upperbound = lowerbound + ((i) * n_section);
+			else if (i > 1 && i < n){
+				sub_lowerbound = lowerbound + ((i-1) * n_section);
+				sub_upperbound = lowerbound + (i * n_section) - 1;
 			}
-			
 			else if (i == n){
-				sub_lowerbound = lowerbound + ((i-1) * n_section) + 1;
+				sub_lowerbound = lowerbound + ((i-1) * n_section);
 				sub_upperbound = upperbound;
 			}
+			
 			printf("Child %d checking range [%d, %d]\n", getpid(), sub_lowerbound, sub_upperbound);
+
+			
+			int memory_block_index = i - 1;
+			int shm_start_ind = memory_block_index * MAX_PRIMES_PER_CHILD;
+			int shm_end_ind = (memory_block_index + 1) * MAX_PRIMES_PER_CHILD - 1;
+
+			//checking if numbers in each range are prime numbers
+			for(int number = sub_lowerbound; number <= sub_upperbound; number++){
+				if( is_prime(number) == 1){
+					shm_ptr[shm_start_ind] = number;
+					printf("%d ", number); 
+					shm_start_ind++;
+					if (shm_start_ind > shm_end_ind) break;
+				}
+			}
+			printf("\n");	
             exit(0);
 		}
+	}
 
 	// Parent waits for all n number of children
     for (int i = 0; i < n; i++) {
