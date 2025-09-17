5a4f5f52845403aa5763fe191110ce559e8c4b76
samiha030331 <145374121+samiha030331@users.noreply.github.com>
2025-09-17 12:23:35 -0600

    Update a1p2.c

--- assignment1/a1p2.c
+++ assignment1/a1p2.c
@@ -6,21 +6,27 @@
 #include <sys/wait.h>
 #include <math.h>
 
+#define MAX_PRIMES_PER_CHILD 1000000 //ask about what range should be appropriate
+
+// function to test if input is a prime number
+int is_prime(int num) {
+	if (num < 2) return 0;
+	for (int i = 2; i <= sqrt(num); i++) {
+		if (num % i == 0) return 0;
+	}
+	return 1;
+}
+
 int main() {
-	//memory
-    //key_t key = ftok("shmfile", 10); //unique key for shared memory
-    //int shmid = shmget(key, 1024, IPC_CREAT | 0666); //from sharedMem.c
-    //int *shm_ptr = (int *) shmat(shmid, NULL, 0); //assignment code
-
-	//parent child communication
-	int toParent;
-	int fromChild;
-
+	//memory layout suggested by assignment sheet
+	int shm_size =  (n * MAX_PRIMES_PER_CHILD) * sizeof(int) // shared memory segment size
+	int shmid = shmget(IPC_PRIVATE, SIZE, IPC_CREAT | 0666); // shared memory creation
+	int *shm_ptr = (int *) shmat(shmid, NULL, 0) // attaches shared memory block
+	
 	//example input
 	int upperbound = 1000; 
 	int lowerbound = 100; 
 	int n = 3; 
-
 
 	//logic for assigning ranges for each process
 	int range = upperbound - lowerbound;
@@ -33,12 +39,11 @@ int main() {
 	for(i = 1; i <= n; i++){
 		
 		pid_t pid = fork(); //Child Process
-
+		
 		if (pid < 0){
 			fprintf(stderr,"Fork Failed");
 			exit(1);
 		}
-
 		else if(pid == 0){
 
 			if(i == 1){
@@ -58,15 +63,6 @@ int main() {
 			printf("Child %d checking range [%d, %d]\n", getpid(), sub_lowerbound, sub_upperbound);
             exit(0);
 		}
-
-    // from assignment page
-    // int is_prime(int num) {
-    //     if (num < 2) return 0;
-    //         for (int i = 2; i <= sqrt(num); i++) {
-    //             if (num % i == 0) return 0;
-    //         }
-    //     return 1;
-    //   }
 
 	// Parent waits for all n number of children
     for (int i = 0; i < n; i++) {
