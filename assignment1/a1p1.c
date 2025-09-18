556b7925234d14f5823a5168e6bb54300b486036
Karamvir623 <karamvir623@gmail.com>
2025-09-18 13:26:58 -0600

    Final code without comments (changed to waitpid)

--- assignment1/a1p1.c
+++ assignment1/a1p1.c
@@ -2,27 +2,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/wait.h>
+#include <sys/wait.h> //USED for 
 #include <stdlib.h>
 
-int main(int arg_c, char **pp_arg_v)
+
+int main()
 {
+
 	pid_t pid, pid_child_search;
+	pid_t pid_list[100];
 	int fromChild;
 	char delimiter_value ;
 	int exit_value;
-	char input_matrix[100][1000];
-	for (int i = 0; i < 3; i++) {
-        printf("argv[%d] = %s\n", i, pp_arg_v[i]);
-    }
+	int input_matrix[100][1000];
+	int matrix_row = 0;
+	int matrix_col = 0;
+	int treasure_check = 0, treasure_col, treasure_row; 
+
 	
 
 	for(int row =0; row<100; row++){
-		printf("ROW: %d\n", row);
 		for(int col =0; col<1000; col++){
-			if(scanf("%c%c", &input_matrix[row][col], &delimiter_value) == 1) printf("ERROR");
-			printf("%c%c", input_matrix[row][col], delimiter_value);
-			
+			if(scanf("%d", &input_matrix[row][col]) != 1) {
+				fprintf(stderr, "Invalid Input file\n");
+				exit(-1);
+			}
+
 		 }
 		
 	}
@@ -30,28 +35,28 @@ int main(int arg_c, char **pp_arg_v)
 	
 
 
-	int matrix_row = 0;
-	int matrix_col = 0;
+	
 	while(matrix_row != 100){
 		pid = fork();
 		if (pid < 0) {
-			fprintf(stderr, "Fork Failed");
+			fprintf(stderr, "Fork for Child %d failed", matrix_row);
 			exit(-1);
 		}
 		else if (pid == 0){
 			matrix_col = 0;
-			exit_value = 0;
+			exit_value = 0; 
 			printf("Child %d (PID %d): Searching row %d\n", matrix_row, getpid(), matrix_row);
 			
 			// sleep(1); // allow parent to wait
 			// printf("Enter an integer between 0 and 255: ");
 			while(matrix_col != 1000){
 				//printf("%c%c", matrix_value, delimiter_value);
-				if(input_matrix[matrix_row][matrix_col] == '1') {
-					printf("value= %c,", input_matrix[matrix_row][matrix_col]);
+				if(input_matrix[matrix_row][matrix_col]) {
+					printf("value= %d,", input_matrix[matrix_row][matrix_col]);
 					printf(" COLUMN = %d\n", matrix_col);
 					printf(" row = %d\n", matrix_row);
-					exit_value = matrix_col;	
+					exit_value = 1;	
+					
 				}
 				
 				matrix_col++;
@@ -61,28 +66,33 @@ int main(int arg_c, char **pp_arg_v)
 			exit(exit_value);
 
 		}
+		else {
+			pid_list[matrix_row] = pid;
+		}
 		
 		matrix_row++;
 		
 	}
-	int treasure_row = 0;
-	int treasure_col;
-	sleep(1);
-	while (pid > 0) {
-		pid_child_search = wait(&fromChild);
-		treasure_col = WEXITSTATUS(fromChild); 
-		if(treasure_col > 0) {
-			printf("Parent: The treasure was found by child with PID %d at row %d and column %d\n", pid_child_search, treasure_row,treasure_col);
-			break;
+	
+	for(int row =0 ; row < 100; row++) {
+		waitpid(pid_list[row], &fromChild, 0);
+		if(WEXITSTATUS(fromChild)){
+			for(int col =0 ; col < 1000; col++) {
+				if(input_matrix[row][col]) {
+					treasure_col = col;
+					treasure_row = row;
+					treasure_check = 1;
+					break;
+				}
+			}
 		}
-		treasure_row++;
-		
 	
 	}
-	
+	if(!treasure_check) fprintf(stderr, "Parent: No treasure found within the input matrix\n");
+	else printf("Parent: The treasure was found by child with PID %d at row %d and column %d\n", pid_list[treasure_row], treasure_row,treasure_col);
 	
 	
 	
 	printf("The parent is done\n");
-	exit(0);
+	return 1;
 }
