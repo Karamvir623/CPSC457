a493125ea1caf1fbdf02b003889c6f2d75cae778
samiha030331 <145374121+samiha030331@users.noreply.github.com>
2025-09-18 19:12:17 -0600

    Update a1p2.c - removing sqrt

--- assignment1/a1p2.c
+++ assignment1/a1p2.c
@@ -11,7 +11,7 @@
 // function to test if input is a prime number
 int is_prime(int num) {
 	if (num < 2) return 0;
-	for (int i = 2; i <= sqrt(num); i++) {
+	for (int i = 2; i*i <= num; i++) {
 		if (num % i == 0) return 0;
 	}
 	return 1;
